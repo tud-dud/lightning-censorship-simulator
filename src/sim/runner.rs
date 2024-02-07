82323cb834e44c70ea6661d8369ad670ecac4540
Charmaine Ndolo <charmaine.ndolo@tu-dresden.de>
2024-02-07 10:27:35 +0100

    rework all packets being dropped

--- src/sim/runner.rs
+++ src/sim/runner.rs
@@ -67,15 +67,9 @@ impl SimBuilder {
             ..Default::default()
         };
         let mut sim_results = vec![];
-        let mut sim_graph = self.graph.clone();
-        if self.drop_strategy == PacketDropStrategy::All {
-            for node in nodes.iter() {
-                sim_graph.remove_node(node);
-            }
-        }
         let mut sim = Simulation::new(
             run,
-            sim_graph.clone(),
+            self.graph.clone(),
             self.amt_msat,
             RoutingMetric::MinFee,
             PaymentParts::Split,
@@ -83,23 +77,28 @@ impl SimBuilder {
             &[],
         );
         let sim_result = sim.run(pairs.to_owned(), None, false);
-        if self.drop_strategy == PacketDropStrategy::IntraProbability {
-            let (updated_results, num_nodes) = if let Some(ratios) = ratios {
-                (
-                    Self::apply_prob_drop_strategy(sim_result, ratios, nodes),
-                    usize::MAX,
-                )
+        let (updated_results, num_nodes) =
+            if self.drop_strategy == PacketDropStrategy::IntraProbability {
+                if let Some(ratios) = ratios {
+                    (
+                        Self::apply_prob_drop_strategy(sim_result, ratios, nodes),
+                        usize::MAX,
+                    )
+                } else {
+                    (sim_result, nodes.len())
+                }
             } else {
-                (sim_result, nodes.len())
+                (
+                    Self::apply_all_dropped_strategy(sim_result, nodes),
+                    nodes.len(),
+                )
             };
-            sim_results.push(SimResult::from_simlib_results(updated_results, num_nodes));
-        } else {
-            // TODO: Should we hand this case differently? thinking we need to fail the
-            // payments after execution
-            sim_results.push(SimResult::from_simlib_results(sim_result, nodes.len()));
-        }
+        sim_results.push(SimResult::from_simlib_results(updated_results, num_nodes));
         summary.sim_results = sim_results;
-        info!("Completed attack by AS {} simulation.", asn);
+        info!(
+            "Completed simulation of {:?} attack by AS {}.",
+            self.drop_strategy, asn
+        );
         summary
     }
 
@@ -122,7 +121,6 @@ impl SimBuilder {
             if Self::payment_involves_asn(&p, asn_nodes) {
                 if let Some(prob) = ratios.choose(&mut rng) {
                     let payment_fate = rng.gen_bool(*prob as f64);
-                    //println!("payment {:?}, fate {}", p, payment_fate);
                     if payment_fate {
                         // dropped
                         p.succeeded = false;
@@ -141,6 +139,35 @@ impl SimBuilder {
                 }
             } else {
                 // no choice to make here
+                updated_results.num_succesful += 1;
+                updated_results.successful_payments.push(p);
+            }
+        }
+        updated_results
+    }
+
+    /// All packets involving the AS's nodes are dropped
+    fn apply_all_dropped_strategy(
+        sim_result: simlib::SimResult,
+        asn_nodes: &[ID],
+    ) -> simlib::SimResult {
+        let mut updated_results = simlib::SimResult {
+            num_failed: sim_result.num_failed,
+            num_succesful: 0,
+            total_num: sim_result.total_num,
+            successful_payments: vec![],
+            failed_payments: sim_result.failed_payments,
+            ..Default::default()
+        };
+        for mut p in sim_result.successful_payments {
+            if Self::payment_involves_asn(&p, asn_nodes) {
+                // dropped
+                p.succeeded = false;
+                p.used_paths = vec![];
+                updated_results.num_failed += 1;
+                updated_results.failed_payments.push(p);
+            } else {
+                // does not involve any AS node so leave as is
                 updated_results.num_succesful += 1;
                 updated_results.successful_payments.push(p);
             }
@@ -273,11 +300,78 @@ mod tests {
         assert_eq!(actual.total_num, sim_result.total_num);
         assert_eq!(actual.total_num, actual.num_succesful + actual.num_failed);
         assert!(actual.num_failed > sim_result.num_failed);
+        assert_eq!(actual.num_failed, 3);
         assert_eq!(actual.num_failed, actual.failed_payments.len());
+
         let ratios = vec![0.0]; // no additional failures
         let actual = SimBuilder::apply_prob_drop_strategy(sim_result.clone(), &ratios, &asn_nodes);
         assert_eq!(actual.total_num, sim_result.total_num);
         assert_eq!(actual.total_num, actual.num_succesful + actual.num_failed);
         assert_eq!(actual.num_failed, sim_result.num_failed);
+    }
+
+    #[test]
+    fn apply_all_drop() {
+        let asn_nodes = vec!["alice".to_owned()];
+        let mut successful_payment =
+            Payment::new(0, String::from("dina"), String::from("bob"), 1, None);
+        let mut path = simlib::Path::new(String::from("dina"), String::from("bob"));
+        path.hops = VecDeque::from([
+            ("dina".to_string(), 0, 0, "".to_string()),
+            ("chan".to_string(), 0, 0, "c".to_string()),
+            ("bob".to_string(), 0, 0, "".to_string()),
+        ]);
+        successful_payment.succeeded = true;
+        successful_payment.used_paths = vec![CandidatePath::new_with_path(path)];
+        let sim_result = simlib::SimResult {
+            num_succesful: 2,
+            num_failed: 1,
+            total_num: 3,
+            successful_payments: vec![successful_payment.clone(), successful_payment],
+            failed_payments: vec![Payment::new(
+                1,
+                String::from("chan"),
+                String::from("bob"),
+                1,
+                None,
+            )],
+            ..Default::default()
+        };
+        let actual = SimBuilder::apply_all_dropped_strategy(sim_result.clone(), &asn_nodes);
+        assert_eq!(actual.total_num, sim_result.total_num);
+        assert_eq!(actual.num_failed, sim_result.num_failed);
+        assert_eq!(actual.total_num, actual.num_succesful + actual.num_failed);
+        assert_eq!(actual.num_succesful, actual.successful_payments.len());
+        assert_eq!(actual.num_failed, actual.failed_payments.len());
+        let mut successful_payment =
+            Payment::new(0, String::from("dina"), String::from("alice"), 1, None);
+        successful_payment.succeeded = true;
+        let mut path = simlib::Path::new(String::from("dina"), String::from("alice"));
+        path.hops = VecDeque::from([
+            ("dina".to_string(), 0, 0, "".to_string()),
+            ("chan".to_string(), 0, 0, "c".to_string()),
+            ("alice".to_string(), 0, 0, "".to_string()),
+        ]);
+        successful_payment.used_paths = vec![CandidatePath::new_with_path(path)];
+        let sim_result = simlib::SimResult {
+            num_succesful: 2,
+            num_failed: 1,
+            total_num: 3,
+            successful_payments: vec![successful_payment.clone(), successful_payment],
+            failed_payments: vec![Payment::new(
+                1,
+                String::from("dina"),
+                String::from("bob"),
+                1,
+                None,
+            )],
+            ..Default::default()
+        };
+        let actual = SimBuilder::apply_all_dropped_strategy(sim_result.clone(), &asn_nodes);
+        assert_eq!(actual.total_num, sim_result.total_num);
+        assert_eq!(actual.total_num, actual.num_succesful + actual.num_failed);
+        assert!(actual.num_failed > sim_result.num_failed);
+        assert_eq!(actual.num_failed, 3);
+        assert_eq!(actual.num_failed, actual.failed_payments.len());
     }
 }
